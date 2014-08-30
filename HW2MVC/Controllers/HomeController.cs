4f3292e2de83416fab6d47ca47426dbc26fbb63d
blake haas <blakeh@gmail.com>
2014-08-30 12:24:11 -0500

    Full commit

--- HW2MVC/Controllers/HomeController.cs
+++ HW2MVC/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace HW2MVC.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application. Modified viewbag - blake";
 
             return View();
         }
